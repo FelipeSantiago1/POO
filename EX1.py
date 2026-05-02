9e0d4c349c702b93d9244c4fa3fe66ecb3d4e7b3
FelipeSantiago1 <felipesantiago1404@hotmail.com>
2026-05-02 20:07:34 -0300

    Primeiro exercicio

---
+++ EX1.py
@@ -0,0 +1,12 @@
+class calculadora: 
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y 
+    def soma(self):
+        return self.x + self.y
+if __name__ == "__main__":
+    x = int(input("Digite o primeiro número: "))
+    y = int(input("Digite o segundo número: "))
+    calc = calculadora(x, y)
+    print("A soma é:", calc.soma())
+    
