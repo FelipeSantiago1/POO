e27b683e012789c11fd7557c73848fc280971d20
FelipeSantiago1 <felipesantiago1404@hotmail.com>
2026-05-02 20:11:16 -0300

    Método subtração

--- EX1.py
+++ EX1.py
@@ -4,9 +4,12 @@ class calculadora:
         self.y = y 
     def soma(self):
         return self.x + self.y
+    def subtracao(self):
+        return self.x - self.y
 if __name__ == "__main__":
     x = int(input("Digite o primeiro número: "))
     y = int(input("Digite o segundo número: "))
     calc = calculadora(x, y)
     print("A soma é:", calc.soma())
+    print("A subtração é:", calc.subtracao())
     
